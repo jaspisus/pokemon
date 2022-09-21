8fff84c9db13b61c649c1d1047b39fd529ac7d00
Mateusz Jackowski <matjackowski1@gmail.com>
2022-09-21 13:02:32 +0200

    [main] pokemon list fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.scss';
 
-const App = () => (
-	<div className="App">
-		<header className="App-header">
-			<img src={logo} className="App-logo" alt="logo" />
-			<p>
-				Edit <code>src/App.tsx</code> and save to reload.
-			</p>
-			<a
-				className="App-link"
-				href="https://reactjs.org"
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				Learn React
-			</a>
-		</header>
-	</div>
-);
+const App = () => {
+	const [data, setData] = useState<any[]>([]);
+	const [details, setDetails] = useState<any[]>([]);
+
+	useEffect(() => {
+		fetch('https://pokeapi.co/api/v2/pokemon/')
+			.then((response) => response.json())
+			.then((data) => {
+				setData(data.results);
+			});
+	}, []);
+
+	useEffect(() => {
+		if (data.length) {
+			Promise.all(
+				data.map((pokemon) =>
+					fetch(pokemon.url).then((response) => response.json())
+				)
+			).then((data) => {
+				setDetails(data);
+			});
+		}
+	}, [data]);
+
+	if (!details.length) {
+		return <>Loading...</>;
+	}
+
+	return (
+		<div className="App">
+			<ul>
+				{details.map((pokemon, index) => (
+					<li>{`${pokemon.id}, ${pokemon.name}, ${pokemon.types[0].type.name}`}</li>
+				))}
+			</ul>
+		</div>
+	);
+};
 
 export default App;
